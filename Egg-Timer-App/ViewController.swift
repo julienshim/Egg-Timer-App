2e1b448c7696e633e7eb32cbdb41b53a80a5bc1a
julienshim <25723428+julienshim@users.noreply.github.com>
2022-04-16 18:34:25 -0700

    first commit

---
+++ Egg-Timer-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg-Timer-App
+//
+//  Created by Julien Shim on 4/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
